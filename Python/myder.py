257a59391d5790aa4ff8c33c645c584f7cbd3b23
vivannguyen <vivantnguyen@gmail.com>
2018-09-19 15:46:26 -0400

    HW1 matlab and python

--- Python/myder.py
+++ Python/myder.py
@@ -4,6 +4,15 @@ Created on Sun Sep  9 16:50:22 2018
 
 @author: Tom K
 """
+import numpy as np
+from numpy import array
+
+def simpleder(a):
+   # da = np.diff(a)
+    #db = np.diff(a)/np.diff(b) 
+    da = np.gradient(a)
+   # return array(da), array(db)
+    return array(da)
 
 def forwardiff(f,a,b,N):
     h = (b-a)/N
@@ -12,7 +21,7 @@ def forwardiff(f,a,b,N):
         slop = (f(a+k*h)-f(a+(k-1)*h))/h
         g.append(slop)
     return array(g)
-    
+
 def backwardiff(f,a,b,N):
     h = (b-a)/N
     g=[]
@@ -28,3 +37,12 @@ def centraldiff(f,a,b,N):
         slop = (f(a+(k+1/2)*h)-f(a+(k-1/2)*h))/h
         g.append(slop)
     return array(g)
+
+def centraldiffcubic(f,a,b,N):
+    h = (b-a)/N
+    g=[]
+    for k in range(0,N):
+        slop = ((1/24)*f(a+(k-3/2)*h)-(27/24)*f(a+(k-1/2)*h)+(27/24)*f(a+(k+1/2)*h)-(1/24)*f(a+(k+3/2)*h))/h
+        g.append(slop)
+    return array(g)
+
